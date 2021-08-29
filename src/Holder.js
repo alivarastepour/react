892d0ee59ed342a79f30e1f70913c4a6e0344026
alivarastepour <alivaraste@outlook.com>
2021-08-29 14:07:44 -0400

    !important

--- src/Holder.js
+++ src/Holder.js
@@ -2,9 +2,6 @@ import React from "react";
 
 const Holder = (props) => {
     const { img , value } = props
-    const func = (x) => {
-        alert(x)
-    }
     return (
         <article>
             <img className={'ig'} src={img} alt=""/>
