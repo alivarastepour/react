c0a89f7226df480f358c85346853ff3ffd1f63e4
alivarastepour <alivaraste@outlook.com>
2021-08-29 13:38:02 -0400

    import - export

---
+++ src/list.js
@@ -0,0 +1,27 @@
+export const list = [
+
+    /*each child in a list should have a unique 'key' prop*/
+    {
+        id : 1 ,
+        value : 'Publication Manual of the American Psychological Association',
+        img : 'https://images-na.ssl-images-amazon.com/images/I/71tqt4VL%2BdS._AC_UL200_SR200,200_.jpg'
+    }
+    ,
+    {
+        id : 2 ,
+        value : 'It Ends with Us: A Novel',
+        img : 'https://images-na.ssl-images-amazon.com/images/I/71lk10uajiL._AC_UL200_SR200,200_.jpg'
+    }
+    ,
+    {
+        id : 3 ,
+        value : 'Our Class is a Family',
+        img : 'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg'
+    }
+    ,
+    {
+        id : 4 ,
+        value : 'American Marxism',
+        img : 'https://images-na.ssl-images-amazon.com/images/I/81lsmyMcYNL._AC_UL200_SR200,200_.jpg'
+    }
+]
