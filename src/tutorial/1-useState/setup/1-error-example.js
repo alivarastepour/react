bc9cc4fda3ca8da414d3ffc686b14790621ba302
alivarastepour <alivaraste@outlook.com>
2021-08-30 18:50:34 -0400

    error ex

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  function func() {
+    title = 'oi' /*no change*/
+  }
+
+
+
+  let title = 'salam';
+  return <React.Fragment>
+    <h2>{title}</h2>
+    <button className='btn' onClick={func}>change title</button>
+  </React.Fragment>;
 };
 
 export default ErrorExample;
