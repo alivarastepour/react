782933a956112d467790464d8327ead7b1d358dc
alivarastepour <alivaraste@outlook.com>
2021-08-30 19:04:46 -0400

    useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+
+  function func() {
+    if (text === 'random')
+      setText('hello Friend')
+    else
+      setText('random')
+  }
+  // const var1 = useState('salam')[0];
+  // const var2 = useState('salam')[1];
+  // console.log(var1, var2)
+  const [text, setText] = useState('random')
+  return <React.Fragment>
+    <h1>{text}</h1>
+    <button onMouseOver={func} className='btn'>click me</button>
+  </React.Fragment>;
 };
 
 export default UseStateBasics;
