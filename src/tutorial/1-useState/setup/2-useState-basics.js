25aa1c25031a164dc11ab8e8c4fc60404402d5d6
alivarastepour <alivaraste@outlook.com>
2021-08-31 13:50:41 -0400

    useState-array examples-remove and change

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-
   function func() {
-    if (text === 'random')
-      setText('hello Friend')
+    if (text === 'hi, leave me')
+      setText('i said leave me')
+    else if (text === 'i said leave me')
+      setText('rock u')
     else
-      setText('random')
+      setText('hi, leave me')
   }
   // const var1 = useState('salam')[0];
   // const var2 = useState('salam')[1];
   // console.log(var1, var2)
-  const [text, setText] = useState('random')
+  const [text, setText] = useState('hi, leave me')
   return <React.Fragment>
     <h1>{text}</h1>
-    <button onMouseOver={func} className='btn'>click me</button>
+    <button onClick={func} className='btn'>click me</button>
   </React.Fragment>;
 };
 
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const newArray = [
+    {id:1, name:'mmd'},
+    {id:2, name:'reza'},
+    {id:3, name:'ali'},
+    {id:4, name:'ho3ein'},
+  ]
+  function func(id){
+    const x = people.filter((c) => c.id !== id)
+    setPeople(x)
+  }
+
+  const [people, setPeople] = useState(data)
+  return <>
+    {
+     people.map((person) =>{
+       const {id, name} = person
+       return <div key={id} className='item'>
+         <h4>{name}</h4>
+         <button className='btn' onClick={() => func(id)}>remove</button>
+       </div>
+     })
+    }
+    <button className='btn' onClick={() => setPeople(newArray)}>change</button>
+  </>;
 };
 
 export default UseStateArray;
+//to clear the list we just use []
