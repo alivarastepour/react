df567242d03f9a5acb2e367a864f0988e530b399
alivarastepour <alivaraste@outlook.com>
2021-09-03 13:09:36 -0400

    onChange event example

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,38 +1,13 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/alivarastepour';
+import React, { useState } from 'react';
 const MultipleReturns = () => {
-  const [isLoading, setLoading] = useState(true)
-  const [isError,setError] = useState(false)
-  const [user, setUser] = useState([])
-  const [ec, setEC] = useState(0)
+  const [value, setValue] = useState('')
+  return (
+      <div>
+        <h1>{value}</h1>
+        <input className='form' placeholder='type to see the result' onChange={(e)=> setValue(e.target.value)} type="text"/>
+      </div>
+  )
 
-  useEffect(()=>{
-    setLoading(true)
-    fetch(url).then((x)=>{
-      if (x.status >= 200 && x.status <= 299){
-        return x.json()
-      }
-      setEC(x.status)
-      setLoading(false)
-      setError(true)
-    }).then((u) => {
-      const {login} = u
-      setUser(login)
-      setLoading(false)
-    }).catch(error => setError(true))
-  } ,[])
-
-  if (isLoading)
-    return <div>
-      <h1>Loading...</h1>
-    </div>
-  if (isError)
-    return <div>
-      <h2>ERROR {ec}</h2>
-    </div>;
-  return <div>
-    <h1>{user}</h1>
-  </div>
 };
 
 export default MultipleReturns;
