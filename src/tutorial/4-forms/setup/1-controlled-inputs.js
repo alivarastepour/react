6698d4f554f2e538f83a297fb787ad698b77f386
alivarastepour <alivaraste@outlook.com>
2021-09-05 11:29:53 -0400

    react form basics - example 1(for proper solution visit final folder)

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -9,14 +9,28 @@ const ControlledInputs = () => {
 
   const [firstName, setFirstName] = useState('')
   const [email, setEmail] = useState('')
-  const handleChange = (e) => {/*e is the event object. preventDefault stops browser from reloading*/
-    e.preventDefault()
+  const [people, setPeople] = useState([])
+  const handleSubmit = (e) => {/*e is the event object. preventDefault stops browser from reloading*/
+    e.preventDefault();
+    if (firstName && email){
+      // const person = {firstName:firstName, email : email}
+      // const person = {firstName, email}this line is similar to the above one
+      const person = {id: new Date().getTime().toString(),firstName, email};
+      // setPeople((people) => {
+      //   return [...people, person];
+      // });
+      setPeople((people)=>{
+        return [...people, person];
+      });
+      setEmail('');
+      setFirstName('');
+    }
 
   }
 
   return <>
     <article>
-      <form className='form' onSubmit={handleChange}>{/*attention here as well*/}
+      <form className='form' onSubmit={handleSubmit}>{/*attention here as well*/}
         <div className='form-control'>
           <label htmlFor="firstName">name : </label>
           <input type="text" name="firstName" id="firstName" onChange={(e) => setFirstName(e.target.value)} value={firstName}/>
@@ -28,7 +42,22 @@ const ControlledInputs = () => {
         <button type='submit'>add person</button>
         {/*<button type='submit' onClick={handleChange}>add person</button>*/} {/*you can submit a form like this. remember that type should be submit*/}
       </form>
+      {
+        people.map((x) => {
+          const [id, name, email ] = x ;
+          return (
+              <div className='item' key={id}>
+                <h1>{name}</h1>
+                <h2>{email}</h2>
+              </div>
+
+          )
+        })
+      }
     </article>
+
+
+
   </>;
 };
 
