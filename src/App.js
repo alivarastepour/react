43489b948e678181f3905b2978da138f10f48a3f
alivarastepour <alivaraste@outlook.com>
2021-09-05 11:48:14 -0400

    react form - spread operator - handling multiple inputs with one state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/ref forwarding/1-ref-forwarding';
+import Setup from './tutorial/4-forms/setup/2-multiple-inputs';
 function App() {
   return (
     <div className='container'>
