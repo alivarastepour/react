309be208a7b55ce03920bce620fdd2bed0920983
alivarastepour <alivaraste@outlook.com>
2021-09-08 11:51:21 -0400

    useReducer startUp

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,39 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+
+  const [people, setPeople] = useState(data);
+  const [show,setShow] = useState(false)
+  const [name, setName] = useState('')
+
+  const handler = (e) => {
+    e.preventDefault();
+    if (name){
+      setShow(true)
+      setPeople([...people,{id: Math.random() * 100 , name}])
+      setName('')
+    }else{
+      setShow(true)
+    }
+  }
+
+  return <>
+
+    {show && <Modal/>}
+    <form onSubmit={handler} className='form'>
+      <div>
+        <input value={name} onChange={(e)=>setName(e.target.value)} type="text"/>
+      </div>
+      <button type='submit'>add</button>
+    </form>
+    {
+      people.map((p)=>{
+        return <div key={p.id}>
+          <h4>{p.name}</h4>
+        </div>
+      })
+    }
+  </>;
 };
 
 export default Index;
