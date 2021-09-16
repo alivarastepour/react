f2eb2e6bc761172f9eb113ef137b69b72aa3e9f0
alivarastepour <alivaraste@outlook.com>
2021-09-16 11:27:10 -0400

    react-Router useParams and placeHolders

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { data } from '../../../data';
 import { Link, useParams } from 'react-router-dom';
 const Person = () => {
+    const [name, setName] = useState('name')
+    const {id} = useParams();
+    useEffect(()=>{
+        const np = data.find((p)=>p.id === parseInt(id));
+        setName(np.name);
+    },[])
   return (
     <div>
-      <h2>person</h2>
+      <h1>{name}</h1>
+        <Link to='/people'>People</Link>
     </div>
   );
 };
