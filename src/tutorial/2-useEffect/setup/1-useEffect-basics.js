4b3941a0aa3cd04a1a9bc74057d767e78f5c1fc6
alivarastepour <alivaraste@outlook.com>
2021-09-20 11:05:23 -0400

    useEffect practice2 - unsuccessful

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,26 +2,37 @@ import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
-const UseEffectBasics = () => {
-  const [state,setState] = useState(true);
-  const [value,setValue] = useState('');
 
-
-
+const useTimer = (reset,ss) => {
+  let [value,setValue] = useState(0);
   useEffect(()=>{
-    if (state){
-      setValue('salam');
-    }else{
-      setValue('oodafes');
+    if (reset){
+      setValue(0);
+      ss(true)
     }
-  },[state])
+    // setInterval(()=>setValue(++value), 1000);
+  },[reset,[]])
+  return value ;
+}
+
+const UseEffectBasics = () => {
+  let [s,ss] = useState(false);
+  let value = useTimer(s,ss);
+
+  const reset = () => {
+    // value = useTimer() ;
+  }
 
   return <>
 
+  <div>
     <h1>{value}</h1>
-    <button onClick={()=>setState(!state)} className="btn">change state</button>
+    <button onClick={()=>ss(true)} className='btn'>reset</button>
+  </div>
 
   </>
+
+
 };
 
 
