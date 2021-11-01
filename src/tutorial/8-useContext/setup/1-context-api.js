de0e14031169f947a2a1db3b1a02f7193cebfd60
alivarastepour <alivaraste@outlook.com>
2021-11-01 18:07:52 -0400

    useContext with dynamic value

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -1,14 +1,16 @@
-import React, {createContext, useContext} from "react";
+import React, {createContext, useContext, useState} from "react";
 
 
 const myContext = createContext("salam");
 
 const App = () => {
-    return (
-        <myContext.Provider value='salamm'>
+    const [v,s] = useState();
+    return <>
+        <input value={v} onChange={(e) => s(e.target.value)} type="text" className="form"/>
+        <myContext.Provider value={v}>
             <User/>
         </myContext.Provider>
-    )
+    </>
 }
 
 const User = () => {
