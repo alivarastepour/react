811980d70cdd73c33f5fc4f0d60ce6c40af5a615
alivarastepour <alivaraste@outlook.com>
2021-11-01 18:14:57 -0400

    prev example with props drilling

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -1,28 +1,49 @@
 import React, {createContext, useContext, useState} from "react";
 
 
-const myContext = createContext("salam");
+// const myContext = createContext("salam");
+//
+// const App = () => {
+//     const [v,s] = useState();
+//     return <>
+//         <input value={v} onChange={(e) => s(e.target.value)} type="text" className="form"/>
+//         <myContext.Provider value={v}>
+//             <User/>
+//         </myContext.Provider>
+//     </>
+// }
+//
+// const User = () => {
+//     return (
+//         <Avatar/>
+//     )
+// }
+//
+// const Avatar = () => {
+//     const value = useContext(myContext);
+//     return (
+//         <h1>{value}</h1>
+//     )
+// }
 
-const App = () => {
-    const [v,s] = useState();
+function App () {
+    const [s,v] = useState();
     return <>
-        <input value={v} onChange={(e) => s(e.target.value)} type="text" className="form"/>
-        <myContext.Provider value={v}>
-            <User/>
-        </myContext.Provider>
-    </>
-}
 
-const User = () => {
+        <input value={s} onChange={(e)=>v(e.target.value)} type="text" className="form"/>
+        <User text={s}/>
+
+    </>
+
+}
+function User (props) {
     return (
-        <Avatar/>
+        <Avatar text={props.text} />
     )
 }
-
-const Avatar = () => {
-    const value = useContext(myContext);
+function Avatar (props) {
     return (
-        <h1>{value}</h1>
+        <h1>{props.text}</h1>
     )
 }
 
