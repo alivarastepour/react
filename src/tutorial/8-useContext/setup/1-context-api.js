ee836cd9c2268b40e8dcc1ec97348f05bb75f129
alivarastepour <alivaraste@outlook.com>
2021-11-01 18:03:04 -0400

    useContext with more nested components

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -5,18 +5,24 @@ const myContext = createContext("salam");
 
 const App = () => {
     return (
-        <myContext.Provider value='salam'>
+        <myContext.Provider value='salamm'>
             <User/>
         </myContext.Provider>
     )
 }
 
 const User = () => {
-    console.log(myContext)
-    const value = useContext(myContext)
+    return (
+        <Avatar/>
+    )
+}
+
+const Avatar = () => {
+    const value = useContext(myContext);
     return (
         <h1>{value}</h1>
     )
 }
+
 
 export default App;
