ad2b8588729a20a835595a3aeb7dcea76ac9e667
alivarastepour <alivaraste@outlook.com>
2021-11-05 18:33:27 -0400

    back to normal hierarchy

--- src/tutorial/15-portals/portals.js
+++ src/tutorial/15-portals/portals.js
@@ -1,15 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {createPortal} from "react-dom";
 
-const Setup = (props) => {
-    const [value, setValue] = useState()
-    const elements =
-        <div>
-            <input className='form' value={value} onChange={(e) => setValue(e.target.value)} type="text"/>
-            <div className='modal'>{value}</div>
-        </div>
-    return createPortal(elements, document.querySelector('#portal-root'))
-}
 
 
 
