fea49432118ed3e448d5ff53111699b253c0ad63
alivarastepour <alivaraste@outlook.com>
2021-11-07 19:48:27 -0500

    typescript interface - javascript object

--- src/tutorial/18-typescript/typescript.tsx
+++ src/tutorial/18-typescript/typescript.tsx
@@ -6,6 +6,10 @@ const Setup = () => {
         state: string,
         hour: number
     }
+    const f = {
+        state:'',
+        hour:0
+    }
 
     const getHour = ():number => {
         const date = new Date();
@@ -14,11 +18,15 @@ const Setup = () => {
     const sample = (num: number):string => {
         return num > 18 ? 'bye' : 'hello' ;
     }
-    const temp:d = {state:sample(getHour()), hour:getHour()}
+    const temp:d = {state:sample(getHour()), hour:getHour()};
+    const temp1 = {state:sample(getHour()), hour:getHour()}
 
     return <>
+        <h3>ts interface:</h3>
         <h2>{temp.state}. it's {temp.hour}</h2>
-    </>
+        <h3>js object:</h3>
+        <h2>{temp1.state}. it's {temp1.hour}</h2>
+    </>;
 }
 
 export default Setup;
