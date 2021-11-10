c2f17614dd89283d793f15539e6e63b8d5edd33f
alivarastepour <alivaraste@outlook.com>
2021-11-10 10:56:08 -0500

    inline styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/20-uncontrolled-component/uncontrolledComponent'
+import Setup from './tutorial/21-inline-styling/inline-styling'
 function App() {
   return (
     <div className='container'>
---
+++ src/tutorial/21-inline-styling/inline-styling.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Setup = () => {
+    const x = {
+        letterSpacing: 10,
+        color: 'red',
+    }
+  return <>
+
+      <div style={x}>salam, mn az inline style estefade miknm</div>
+  </>
+}
+
+export default Setup;
