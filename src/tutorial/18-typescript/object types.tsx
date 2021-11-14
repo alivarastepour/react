b223f2f7e0f57d809f47dee6651ab0217bf3dba9
alivarastepour <alivaraste@outlook.com>
2021-11-13 21:42:11 -0500

    optional parameters

--- src/tutorial/18-typescript/object types.tsx
+++ src/tutorial/18-typescript/object types.tsx
@@ -19,6 +19,10 @@ const Setup = () => {
         return person.age;
     }
 
+    function f3(person: {age?:number, name:string}) : string{
+        return person.name + ' is ' + person.age;
+    }
+
     const o1:a = {name:'mamad', age:13};
     const o2:b = {name:'zohre', age:53};
 
@@ -28,6 +32,14 @@ const Setup = () => {
         <h5>{f({name:'ali', age:12})}</h5>
         <h5>{f1(o1)}</h5>
         <h5>{f2(o2)}</h5>
+        <h4>object properties can be optional</h4>
+        <p>both parameters :
+            {f3({name:"reza", age:43})}
+        </p>
+        <p>
+            only name parameter :
+            {f3({name: 'hadi'})}
+        </p>
     </>
 
 }
