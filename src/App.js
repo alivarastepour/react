308c2f1f373ca1af4058cb65704f348a5ded0ab7
Ali <alivaraste@outlook.com>
2021-11-15 15:44:51 +0330

    generic object type

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/23-ESLint/eslint'
+import Setup from './tutorial/18-typescript/generic object type'
 function App() {
   return (
     <div className='container'>
