a8fb53ef12b5887e73cc3401104c929be01934d1
alivarastepour <alivaraste@outlook.com>
2021-11-26 09:19:40 -0500

    styled components example : theme switch

--- src/tutorial/24-styled-components/index.js
+++ src/tutorial/24-styled-components/index.js
@@ -1,12 +1,32 @@
-import React from "react";
-import {Spinner} from "./styles";
+import React, {useState} from "react";
+import {Wrapper} from "./styles";
 
 
 const Setup = () => {
-    return <>
-        <Spinner>
 
-        </Spinner>
+    const [dark, setDark] = useState(false);
+
+    const themeLIGHT = {
+        bgc: 'white',
+        color: '#1c1c1c'
+    }
+    const themeDARK = {
+        bgc: '#1c1c1c',
+        color: 'white'
+    }
+    return <>
+        <Wrapper color={dark ? themeDARK : themeLIGHT} >
+            <div>
+                hello world
+            </div>
+            <h3>
+                hello class
+            </h3>
+        </Wrapper>
+        <div>
+            <label htmlFor="x">dark mode</label>
+            <input id='x' value={dark} onChange={e=>setDark(e.target.checked)} type="checkbox" className="form"/>
+        </div>
     </>
 }
 export default Setup
--- src/tutorial/24-styled-components/styles.js
+++ src/tutorial/24-styled-components/styles.js
@@ -1,21 +1,7 @@
 import styled from "styled-components";
 
-export const Spinner = styled.div`
-  width: 50px;
-  height: 50px;
-  border-bottom: none;
-  border-top: 2px black solid;
-  border-left: 2px black solid;
-  border-radius: 50%;
-  animation: spin 1s linear infinite; 
-  @keyframes spin{
-    0% {
-      transform: rotate(0deg);
-    }
-    100%{
-      transform: rotate(360deg);
-     }
-  }
-  
+export const Wrapper = styled.div`
+    color: ${(props) => props.color.color};
+    background-color: ${(props) => props.color.bgc} ;
 `
 
