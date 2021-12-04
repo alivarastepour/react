2422ada0afef32efd851ae3b910a9bc14b114652
ali varaste pour <alivaraste@outlook.com>
2021-12-04 01:24:21 -0700

    framer-motion spinner is costumized

--- src/tutorial/27-react-with-framer-motion/index.js
+++ src/tutorial/27-react-with-framer-motion/index.js
@@ -1,19 +1,16 @@
 import * as React from "react";
 import { useState } from "react";
 import { motion } from "framer-motion";
-import { Refresh } from "./refresh";
 
 import "./styles.css";
 
 const Setup = () => {
-  const [count, setCount] = useState(0);
   return (
     <>
-      <Refresh onClick={() => setCount(count + 1)} />
       <div className="example-container">
         <motion.div
           animate={{
-            scale: [1, 2, 2, 1, 1],
+            scale: [0.5, 1, 1.5, 1, 0.5],
             rotate: [0, 0, 270, 270, 0],
             borderRadius: ["20%", "20%", "50%", "50%", "20%"]
           }}
