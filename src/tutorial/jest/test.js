d9a16aa94d9255b418ac7111a1937b8f280f36db
ali varaste pour <alivaraste@outlook.com>
2021-12-10 13:11:09 -0700

    jest object test

--- src/tutorial/jest/test.js
+++ src/tutorial/jest/test.js
@@ -1,5 +1,7 @@
 const sum = require('./sum')
 const a = [1,2,3,4,5,6,7,8,9,0];
+const b = {'a':2, 'b':44};
+const c = {'a':2, 'b':44, 'c':21};
 test('wtf', () => {
-    expect(a).toContain(12);
+    expect(b).toEqual(c);
 })
