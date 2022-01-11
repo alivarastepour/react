6d7d3946ab4add5c269de66221cec7c2acedf382
ali varaste pour <alivaraste@outlook.com>
2022-01-11 00:16:02 -0700

    MUI InputLabel

--- src/tutorial/28-MUI/Select.js
+++ src/tutorial/28-MUI/Select.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Select } from "@mui/material";
 import { MenuItem } from "@mui/material";
+import { FormControl, InputLabel } from "@mui/material";
 
 const Setup = () => {
     return <>
-        <Select placeholder="salamp" variant="standard" sx={{width:300}} 
+    <FormControl>
+        <InputLabel>چن سالدس عمو؟</InputLabel>
+    <Select placeholder="salamp" variant="standard" sx={{width:300}} 
         label="salam">
             <MenuItem value={1}>1</MenuItem>
             <MenuItem value={2}>2</MenuItem>
             <MenuItem value={3}>3</MenuItem>
             <MenuItem value={4}>4</MenuItem>
         </Select>
+    </FormControl>
+
     </>
 }
 export default Setup
