b22f55749dcaca15adabe33d56fe53509be2ddc3
ali varaste pour <alivaraste@outlook.com>
2022-02-05 05:40:18 -0700

    MUI Container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/28-MUI/Box";
+import Setup from "./tutorial/28-MUI/Container";
 function App() {
   return (
     <div className="container">
---
+++ src/tutorial/28-MUI/Container.js
@@ -0,0 +1,23 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+const Setup = () => {
+  return (
+    <>
+      <Container maxWidth="sm" fixed>
+        <div>
+          salam salam salam salam salam salam salam salam salam salam salam
+          salam salam salam salam salam salam salam salam salam salam salam
+          salam salam salam salam salam salam salam salam salam salam salam
+          salam salam salam salam salam salam salam salam salam salam salam
+          salam salam salam salam salam salam salam salam salam salam salam
+          salam salam salam salam salam salam salam salam salam salam salam
+          salam salam salam salam salam salam salam salam salam salam salam
+          salam salam salam salam salam salam salam salam salam salam salam
+          salam salam
+        </div>
+      </Container>
+    </>
+  );
+};
+export default Setup;
