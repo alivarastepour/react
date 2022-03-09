94c318699c0906e3e10940bcbf289e7087b0e2b9
Ali <alivaraste@outlook.com>
2022-03-09 17:33:34 +0330

    Module design pattern(dynamic import)

--- src/tutorial/DesignPatterns/Module/customModule.js
+++ src/tutorial/DesignPatterns/Module/customModule.js
@@ -1,5 +1,5 @@
 export function add(a, b) {
-    return a+ b;
+    console.log(a+ b) ;
 }
 
 export function sayHi() {
--- src/tutorial/DesignPatterns/Module/index.js
+++ src/tutorial/DesignPatterns/Module/index.js
@@ -1,27 +1,34 @@
 // import {add, sayBye, sayHi} from './customModule'
-import * as module from './customModule'
+// import * as module from './customModule'
 const Setup = () => {
+    const loadModule = (param, val1, val2) => {
+        import("./customModule").then(a => {
+            if (param === 'add')
+                a.add(val1,val2);
+        })
+    }
     return <>
         {
+            loadModule('add', 1, 2)
             // add(1,5)
-            module.add(2,3)
+            // module.add(2,3)
         }
         {
             <p>
                 {
-                    module.sayHi()
+                    // module.sayHi()
                     // sayHi()
                 }
             </p>
         }
         <span>
             {
-                module.default()
+                // module.default()
             }
         </span>
         <div>
             {
-                module.sayBye()
+                // module.sayBye()
                 // sayBye()
             }
         </div>
